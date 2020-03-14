c30f1c88a08b06995b5f53195ab403f9dc9c4060
Oana Munteanu <hello2oana@yahoo.com>
2020-03-14 13:43:28 +0000

    New solution using byte map

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -8,11 +8,21 @@
 
 package strand
 
-import (
-	"strings"
-)
-
 //ToRNA func takes a given DNA strand as input and return its RNA complement (per RNA transcription
 func ToRNA(dna string) string {
-	return strings.NewReplacer("G", "C", "C", "G", "T", "A", "A", "U").Replace(dna)
+	//convert our string into a rune
+	dnaR := []byte(dna)
+	//initiliaze our RNA
+	//var rna string
+	//initliaze a nucleotides DNA vs RNA map
+	nucleotides := map[byte]byte{
+		'G': 'C',
+		'C': 'G',
+		'T': 'A',
+		'A': 'U',
+	}
+	for i, j := range dnaR {
+		dnaR[i] = nucleotides[j]
+	}
+	return string(dnaR)
 }
