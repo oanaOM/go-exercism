db2ee86ffa154793a962adbcea580986a1967628
Oana Munteanu <hello2oana@yahoo.com>
2020-03-30 13:33:48 +0100

    refactor scrabble word calculator to improve performance

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -11,39 +11,33 @@ Q, Z                               10
 package scrabble
 
 import (
-	"strings"
+	"unicode"
 )
-
-//LetterCount counts how many letters of same type are in a word
-func LetterCount(s string) map[string]int {
-	m := make(map[string]int)
-	for _, w := range []rune(strings.ToLower(s)) {
-		m[string(w)]++
-	}
-	return m
-}
 
 //Score takes a word as parameter and calculates it's scrore
 func Score(word string) int {
 	//initializing our score
 	var score int
-	//create a map having the letter point as key and corresponding letter as value
-	scoreMap := map[int]string{
-		1:  "AEIOULNRST",
-		2:  "DG",
-		3:  "BCMP",
-		4:  "FHVWY",
-		5:  "K",
-		8:  "JX",
-		10: "QZ",
-	}
-	for _, letter := range strings.Split(strings.ToLower(word), "") {
-		for k, v := range scoreMap {
-			if strings.Contains(strings.ToLower(v), letter) {
-				score += k
-			}
-		}
 
+	for _, letter := range word {
+		letter = unicode.ToUpper(letter)
+
+		switch letter {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score++
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
 	}
 	return score
 }
