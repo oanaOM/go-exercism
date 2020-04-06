4a5b23f4efdd688cd8f7c1035a98d956f9cdc6de
Oana Munteanu <hello2oana@yahoo.com>
2020-04-06 21:47:25 +0100

    Add a simple solution using map

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -7,31 +7,26 @@ An isogram (also known as a "nonpattern word") is a word or phrase without a rep
 	six-year-old
 
 non-isogram words: isograms
-
 */
 package isogram
 
 import (
-	"strings"
+	"unicode"
 )
 
 //IsIsogram checks if a word/phrase is isogram
 func IsIsogram(phrase string) bool {
-	//convert to lower case
-	phrase = strings.ToLower(phrase)
-	//replace all hyphen
-	phrase = strings.ReplaceAll(phrase, "-", "")
-	//replace all spaces
-	phrase = strings.ReplaceAll(phrase, " ", "")
 
-	if len(phrase) != 1 && len(phrase) != 0 {
-		for i := 0; i < len(phrase); i++ {
-			for j := i + 1; j < len(phrase); j++ {
-				if phrase[i] == phrase[j] {
-					return false
-				}
-			}
+	runes := map[rune]bool{}
+
+	for _, r := range phrase {
+
+		r = unicode.ToLower(r)
+		if runes[r] && unicode.IsLetter(r) {
+			return false
 		}
+
+		runes[r] = true
 	}
 
 	return true
