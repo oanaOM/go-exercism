5aaaf2947ca113baf15f0cfd60f53833a3de7ceb
Oana Munteanu <hello2oana@yahoo.com>
2020-05-02 18:27:09 +0100

    simple solution using formulas instead.

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,39 +1,14 @@
 package diffsquares
 
-import (
-	"math"
-)
-
-type mathExpr = string
-
-const (
-	squareOfSum  = mathExpr("SquareOfSum")
-	sumOfSquares = mathExpr("SumOfSquares")
-)
-
-//MathExpression returns different results depending on the math expression wanted
-func MathExpression(expr mathExpr, n int) int {
-	var result, sum int
-	for i := 1; i <= n; i++ {
-		switch expr {
-		case squareOfSum:
-			sum += i
-			result = int(math.Pow(float64(sum), 2))
-		case sumOfSquares:
-			result += int(math.Pow(float64(i), 2))
-		}
-	}
-	return result
-}
-
 //SquareOfSum takes as input the first N natural numbers and return the squar root of their sum
 func SquareOfSum(n int) int {
-	return MathExpression(squareOfSum, n)
+	sum := n * (n + 1) / 2
+	return sum * sum
 }
 
 //SumOfSquares takes as input the first N natural numbers and return the sum of the squart root of each number
 func SumOfSquares(n int) int {
-	return MathExpression(sumOfSquares, n)
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 //Difference should return diference between two numbers
